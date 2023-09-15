bca2b0bae770713d91c73dea9b0dec6887425dc0
Accult <acculltt@gmail.com>
2023-09-15 11:20:25 +0300

    All done

---
+++ guess_the_number_game.py
@@ -0,0 +1,18 @@
+import random
+
+
+def chose_random_number():
+    random_number = random.randint(1, 10)
+    for attempt in range(5):
+        user_number = int(input('Guess a number between 1 and 10 includes: '))
+        if user_number == random_number:
+            return f"Congratulations, you guessed right :)"
+        elif user_number < random_number:
+            print("It's too low. Try again!")
+        elif user_number > random_number:
+            print("It's too high. Try again!")
+    return f"You have used all your attempts. Correct number was {random_number} :("
+
+
+start = chose_random_number()
+print(start)
